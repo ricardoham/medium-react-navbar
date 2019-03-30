d4c5f853e6f7f0cd74d55f7d28a6282a092ee38b
Ricardo Manoel <rdomanoel@gmail.com>
2019-03-29 22:22:11 -0300

    add eslint

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,12 @@ import '../styles/navbar.css';
 
 const Navbar = () => (
   <nav className="navbar">
-    <NavLink exact activeClassName="navbar__link--active" className="navbar__link" to="/">Home</NavLink>
+    <NavLink 
+      exact activeClassName="navbar__link--active" 
+      className="navbar__link" to="/"
+      >
+      Home
+    </NavLink>
     <NavLink activeClassName="navbar__link--active" className="navbar__link" to="/products">Products</NavLink>
     <NavLink activeClassName="navbar__link--active" className="navbar__link" to="/contacts">Contacts</NavLink>
   </nav>
